4a6f70bbaacc657fe63efb310850f267c52ee501
UltraHispeed-Elite <62296749+UltraHispeed-Elite@users.noreply.github.com>
2024-02-24 13:52:48 -0600

    Initial commit

---
+++ src/main/java/net/matt/tutorialmod/Tutorial_modClient.java
@@ -0,0 +1,10 @@
+package net.matt.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Tutorial_modClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/matt/tutorialmod/Tutorial_modDataGenerator.java
@@ -0,0 +1,11 @@
+package net.matt.tutorialmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Tutorial_modDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
